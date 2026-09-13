cfcc8cc101cf0f061f74ce9c2e9b91321f3b6d4e
Malik Yasir <my4871767@gmail.com>
2026-09-13 22:16:32 +0500

    file system

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-fs.rename("Hello File", "New File", function (err) {
+fs.unlink("New File", function (err) {
   if (err) console.log(err);
-  else console.log("File renamed successfully");
+  else console.log("File deleted successfully");
 });
