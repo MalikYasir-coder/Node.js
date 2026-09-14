f5853e527b76034a8cc6673f1ea05aa8513b1363
Malik Yasir <my4871767@gmail.com>
2026-09-14 19:28:15 +0500

    file system

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const fs = require("fs");
-fs.unlink("New File", function (err) {
-  if (err) console.log(err);
-  else console.log("File deleted successfully");
+// const fs = require("fs");
+// fs.unlink("New File", function (err) {
+//   if (err) console.log(err);
+//   else console.log("File deleted successfully");
+// });
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.end("Hello World");
 });
+server.listen(3000);
