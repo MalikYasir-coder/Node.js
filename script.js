f8ace453f4362336c672e785ed960fadd99a19cf
Malik Yasir <my4871767@gmail.com>
2026-09-15 20:09:36 +0500

    express js

--- script.js
+++ script.js
@@ -3,8 +3,23 @@
 //   if (err) console.log(err);
 //   else console.log("File deleted successfully");
 // });
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.end("Hello World");
+// const http = require("http");
+// const server = http.createServer((req, res) => {
+//   res.end("Hello World");
+// });
+// server.listen(3000);
+const express = require("express");
+const app = express();
+// Creating Routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
-server.listen(3000);
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+app.get("/contact", (req, res) => {
+  res.send("Contact Page");
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
