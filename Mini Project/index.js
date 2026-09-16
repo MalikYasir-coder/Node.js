8046ea0162794ac76aa4b3320dd52cd675ef07f5
Malik Yasir <my4871767@gmail.com>
2026-09-16 21:09:43 +0500

    Ejs

--- Mini Project/index.js
+++ Mini Project/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 // parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
