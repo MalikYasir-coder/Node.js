fd762ecd0bba3aaf62327cc8a40c466d972c5fcd
Malik Yasir <my4871767@gmail.com>
2026-09-16 17:04:31 +0500

    express js form handling

--- script.js
+++ script.js
@@ -22,6 +22,8 @@
 // });
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
   console.log("User is signed in");
   next();
