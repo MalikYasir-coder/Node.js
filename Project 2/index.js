e21578f30dbe0f5620aaa8a2bb5b13c29665ae6a
Malik Yasir <my4871767@gmail.com>
2026-09-17 17:26:39 +0500

    p2

---
+++ Project 2/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const Path = require("path");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(Path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
