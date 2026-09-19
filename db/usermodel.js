eefe376bf31b63b1c9c6636a2fea8ad6dc29e4cb
Malik Yasir <my4871767@gmail.com>
2026-09-19 19:35:01 +0500

    mongo db

---
+++ db/usermodel.js
@@ -0,0 +1,8 @@
+const moongoose = require("mongoose");
+mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
+const userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+module.exports = mongoose.model("User", userSchema);
