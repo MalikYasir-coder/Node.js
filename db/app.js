e5e989a66afbc0a7cef54e5bc72af442aa61ae12
Malik Yasir <my4871767@gmail.com>
2026-09-22 00:31:14 +0500

    Crud Operations

--- db/app.js
+++ db/app.js
@@ -7,11 +7,27 @@ app.get("/", (req, res) => {
 });
 app.get("/create", async (req, res) => {
   let user = await userModel.create({
-    name: "muhammad yasir",
-    email: "myasirdev135@app.com",
-    password: "password123",
+    name: "malik abdullah",
+    email: "mabdullah@app.com",
+    password: "abdullah123",
   });
   res.send(user);
+});
+app.get("/update", async (req, res) => {
+  let updateduser = await userModel.findOneAndUpdate(
+    { name: "malik abdullah" },
+    { name: "malikyasir" },
+    { new: true },
+  );
+  res.send(updateduser);
+});
+app.get("/read", async (req, res) => {
+  let readuser = await userModel.find();
+  res.send(readuser);
+});
+app.get("/delete", async (req, res) => {
+  let deleteuser = await userModel.findOneAndDelete({ name: "malik abdullah" });
+  res.send(deleteuser);
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
