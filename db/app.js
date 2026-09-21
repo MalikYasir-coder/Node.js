3f8ae5b4de52f5cc6611d9dd9ce428f74600afab
Malik Yasir <my4871767@gmail.com>
2026-09-21 20:55:37 +0500

    mongo db

--- db/app.js
+++ db/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+const userModel = require("./usermodel");
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+app.get("/create", async (req, res) => {
+  let user = await userModel.create({
+    name: "muhammad yasir",
+    email: "myasirdev135@app.com",
+    password: "password123",
+  });
+  res.send(user);
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- db/usermodel.js
+++ db/usermodel.js
@@ -1,6 +1,7 @@
-const moongoose = require("mongoose");
-mongoose.connect(`mongodb://127.0.0.1:27017/mongopractice`);
-const userSchema = mongoose.Schema({
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/mydatabase");
+// create a schema for the user model
+const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
