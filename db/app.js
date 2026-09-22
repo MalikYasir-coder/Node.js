debb242cef6b9108b7d45d5771917ce0ffe602b5
Malik Yasir <my4871767@gmail.com>
2026-09-23 02:32:37 +0500

    Crud Operations with Ejs

--- db/app.js
+++ db/app.js
@@ -9,6 +9,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/read", (req, res) => {
+  res.render("read");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
