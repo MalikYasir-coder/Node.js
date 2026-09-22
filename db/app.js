f263099d21fab1846a9f2137b2f3677b2f5ef2e6
Malik Yasir <my4871767@gmail.com>
2026-09-23 00:53:17 +0500

    Crud Operations with Ejs

--- db/app.js
+++ db/app.js
@@ -1,33 +1,13 @@
 const express = require("express");
 const app = express();
-const userModel = require("./usermodel");
+const path = require("path");
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-app.get("/create", async (req, res) => {
-  let user = await userModel.create({
-    name: "malik abdullah",
-    email: "mabdullah@app.com",
-    password: "abdullah123",
-  });
-  res.send(user);
-});
-app.get("/update", async (req, res) => {
-  let updateduser = await userModel.findOneAndUpdate(
-    { name: "malik abdullah" },
-    { name: "malikyasir" },
-    { new: true },
-  );
-  res.send(updateduser);
-});
-app.get("/read", async (req, res) => {
-  let readuser = await userModel.find();
-  res.send(readuser);
-});
-app.get("/delete", async (req, res) => {
-  let deleteuser = await userModel.findOneAndDelete({ name: "malik abdullah" });
-  res.send(deleteuser);
+  res.render("index");
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
