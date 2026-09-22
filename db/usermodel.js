c4beeeb090e0e94ca57a5992461ed3849667c62a
Malik Yasir <my4871767@gmail.com>
2026-09-22 22:31:17 +0500

    Crud Operations

--- db/usermodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/mydatabase");
-// create a schema for the user model
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-});
-module.exports = mongoose.model("User", userSchema);
