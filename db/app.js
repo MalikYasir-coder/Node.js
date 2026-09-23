ef2d8d28d107fa465a950ad12d390d044043a58c
Malik Yasir <my4871767@gmail.com>
2026-09-23 21:24:16 +0500

    Crud Operations with Ejs

--- db/app.js
+++ db/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const userModel = require("./models/usermodel");
+const usermodel = require("./models/usermodel");
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,8 +11,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.get("/read", (req, res) => {
-  res.render("read");
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+  let createduser = await userModel.create({
+    name,
+    email,
+    image,
+  });
+  res.redirect("/read");
+});
+app.get("/read", async (req, res) => {
+  let users = await userModel.find({});
+  res.render("read", { users });
+});
+app.get("/delete/:id", async (req, res) => {
+  let { id } = req.params;
+  await userModel.findByIdAndDelete(id);
+  res.redirect("/read");
 });
 
 app.listen(3000, () => {
