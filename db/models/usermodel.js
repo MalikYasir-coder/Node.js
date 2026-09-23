cb9db3afc341a111c01d0f6e80e09a24a0925fcc
Malik Yasir <my4871767@gmail.com>
2026-09-23 20:05:08 +0500

    Crud Operations with Ejs

---
+++ db/models/usermodel.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/testapp1");
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  image: String,
+});
+module.exports = mongoose.model("User", userSchema);
