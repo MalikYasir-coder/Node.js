b31e528d789832163827d66505657f3044b6c184
Malik Yasir <my4871767@gmail.com>
2026-09-26 00:54:18 +0500

    Authentication and Authorization

--- Authentication And Authorization/app.js
+++ Authentication And Authorization/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+
 app.get("/", function (req, res) {
   bcrypt.genSalt(10, function (err, salt) {
-    bycrypt.hash("pololololo", salt, function (err, hash) {});
+    bcrypt.hash("password", salt, function (err, hash) {
+      console.log(hash); // yahan use karo, jahan "hash" available hai
+      res.send(hash); // response bhi yahin se bhejo
+    });
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
